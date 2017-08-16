cf6baf5657480cfd4c7274d743fa7dd1aa41f336
Austin Cummings <skwerl_e@yahoo.com>
2017-08-15 23:54:08 -0700

    change index and js, introduce a count and make judgements

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,18 @@
 $(document).ready(function() {
-    $("form#myers-brigg_survey").submit(function(event) {
-        event.preventDefault();
-        $("#work-responses").show();
-        console.log("#work-responses")
-        $("input:checkbox[name=myers-brigg]:checked").each(function() {
-          thinkies = $(this).val();
-          console.log(thinkies);
-        });
-        var userResponses = [];
-        console.log(userResponses);
-
-        for(var i = 0; i < thinkies.length; ++i){
-              if(i===0) {
-              count=0};
-              alert(count);
-            if(thinkies[i] === 1) {
-                count++;
-        } else if (thinkies === -1 ) {
-          count--;
-        };
-        };
-      });
+  $("form#myers-brigg_survey").submit(function(event) {
+    event.preventDefault();
+    $("#mb-responses").show();
+    console.log("#mb-responses")
+    var newArray = [];
+    $("input:checkbox[name=myers-brigg]:checked").each(function() {
+      var pass = $(this).val();
+      newArray.push(pass);
     });
+    var sum=0; var i=newArray.length; while(i--) sum += parseInt(newArray[i])
+    if (sum >=1) {
+      alert("you're an extrovert");
+    } else if (sum < 0) {
+      alert("You're an introvert")
+    }
+  });
+});
